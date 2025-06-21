6be3e2c37473dc0bb34b848e3ac572e002509c2c
loreleysarmiento <sarmientoloreley@gmail.com>
2025-06-20 21:10:05 -0500

    Search Items

---
+++ src/public/components/page-not-found.component.vue
@@ -0,0 +1,41 @@
+<script>
+
+export default {
+  name: "page-not-found",
+
+  data() {
+    return {
+      unavailableRoute: null,
+    }
+  },
+  created() {
+    this.unavailableRoute = this.$route.path;
+  }
+}
+</script>
+
+<template>
+  <div class="w-full">
+    <h1>Pagina no encontrada</h1>
+    <p>La ruta {{ unavailableRoute }} no es valida.</p>
+    <router-link to="/start">Ir al inicio</router-link>
+  </div>
+</template>
+
+<style scoped>
+.w-full{
+  width: 100%;
+  height: 100vh;
+  padding: 2rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+}
+h1{
+  font-size: 4rem;
+}
+p{
+  font-size: 2rem;
+}
+</style>
